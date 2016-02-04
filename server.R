64debf3361b2a44299c4ce5d061393b599a15211
julianhatwell <julian.hatwell@googlemail.com>
2016-02-04 17:21:48 +0800

    added slids

--- server.R
+++ server.R
@@ -36,21 +36,33 @@ shinyServer(
     x <- seq(-1, 1, length.out = 1000)
     
     output$dens <- renderPlot({
-
+      x_null_reject <- x[x > st.err() * qnorm(1 - sig())]
+      x_alt_accept <- x[x + input$delta > st.err() * qnorm(1 - input$power)] +input$delta
       plot(x
+          , dnorm(x, sd = st.err())
+          , type = "n"
+          , ylab = ""
+          , yaxt = "n")
+      polygon(c(x_alt_accept[1],x_alt_accept)
+              , c(0, dnorm(x_alt_accept, mean = input$delta, sd = st.err()))
+              , col = "pink"
+              , border = "transparent") 
+      polygon(c(x_null_reject[1],x_null_reject)
+              , c(0, dnorm(x_null_reject, sd = st.err()))
+              , col = "light blue"
+              , border = "transparent") 
+      points(x
           , dnorm(x, sd = st.err())
           , type = "l"
           , col = "blue"
-          , xlab = "sample means"
-          , ylab = ""
-          , yaxt = "n")
-      abline(v = st.err() * qnorm(1 - sig())
-             , lwd = 3
-             , col = "magenta")
+          , xlab = "sample means")
       points(x + input$delta
              , dnorm(x, sd = st.err())
              , type = "l"
-             , col = "purple")      
+             , col = "purple")
+      abline(v = st.err() * qnorm(1 - sig())
+             , lwd = 3
+             , col = "magenta")
       if (input$alt == "two.sided") {
         points(x - input$delta
                , dnorm(x, sd = st.err())
